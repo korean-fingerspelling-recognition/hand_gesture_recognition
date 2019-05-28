55c1c0920f27c6ae2d7297c08f6b581e27689c85
amy-hyunji <44370759+amy-hyunji@users.noreply.github.com>
2019-05-28 21:33:19 +0900

    Update layers.py

--- layers.py
+++ layers.py
@@ -1,6 +1,8 @@
 import numpy as np
 import tensorflow as tf
-import six
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
 
 def conv2d(inputs, weights, biases, strides=1):
     inputs = tf.nn.conv2d(inputs, weights, [1,1,1,1],padding = 'SAME')
@@ -31,7 +33,7 @@ def convnet(inputs, weights, biases, keep_prob):
     return tf.add(tf.matmul(fc1,weights['out']),biases['out'],name='logits')
 
 def resnet(inputs):
-	with tf.variable_scope("network", reuse=not FLAGS.train_mode):
+	with tf.variable_scope("network", reuse=FLAGS.train_mode):
 		if FLAGS.model_num==2: # in case of deep model
 			residual_block = bottle_resblock
 		else: #FLAGS.model_num==1
@@ -40,12 +42,13 @@ def resnet(inputs):
 		ch = 32
 		x = conv(inputs, ch, 3, 1, "conv")
 		for i in range(2):
+			print("channel number 64: {}".format(i))
 			x = residual_block(x, ch*2, FLAGS.train_mode, True, True) # ch = 128
 		x = residual_block(x, ch*4, FLAGS.train_mode, True, False)
 		for i in range(2):
 			x = residual_block(x, ch*4, FLAGS.train_mode, True, True) # ch = 256
 		x = residual_block(x, ch*8, FLAGS.train_mode, True, False)
-		x = batch_norm(x, FLAGS.train_mode, scope = 'batch_norm')
+		x = batch_norm(x, FLAGS.train_mode, ch*8, scope = 'batch_norm')
 		x = tf.nn.relu(x)
 		# x = global_avg_pooling(x)
 		x = fully_connected(x, units=FLAGS.num_classes, scope='logit')
@@ -55,15 +58,15 @@ def resnet(inputs):
 def resblock(x_init, channels, is_training=True, use_bias=True, 
 		downsample=False, scope='resblock') :
     with tf.variable_scope(scope) :
-        x = batch_norm(x_init, is_training, scope='batch_norm_0')
+ 	f_channel = x_init.get_shape().as_list()[-1]
+        x = batch_norm(x_init, is_training, f_channel, scope='batch_0')
         x = tf.nn.relu(x)
         if downsample :
             x = conv(x, channels, kernel=3, stride=2, use_bias=use_bias, scope='conv_0')
             x_init = conv(x_init, channels, kernel=1, stride=2, use_bias=use_bias, scope='conv_init')
         else :
             x = conv(x, channels, kernel=3, stride=1, use_bias=use_bias, scope='conv_0')
-
-        x = batch_norm(x, is_training, scope='batch_norm_1')
+        x = batch_norm(x, is_training, channels, scope='batch_1')
         x = tf.nn.relu(x)
         x = conv(x, channels, kernel=3, stride=1, use_bias=use_bias, scope='conv_1')
         return x + x_init
@@ -72,12 +75,12 @@ def resblock(x_init, channels, is_training=True, use_bias=True,
 def bottle_resblock(x_init, channels, is_training=True, use_bias=True, 
 							downsample=False, scope='bottle_resblock') :
     with tf.variable_scope(scope) :
-        x = batch_norm(x_init, is_training, scope='batch_norm_1x1_front')
+        x = batch_norm(x_init, is_training, channels, scope='batch_2')
         shortcut = relu(x)
 
         x = conv(shortcut, channels, kernel=1, stride=1, use_bias=use_bias, 
 						scope='conv_1x1_front')
-        x = batch_norm(x, is_training, scope='batch_norm_3x3')
+        x = batch_norm(x, is_training, channels, scope='batch_3')
         x = relu(x)
 
         if downsample :
@@ -92,7 +95,7 @@ def bottle_resblock(x_init, channels, is_training=True, use_bias=True,
             shortcut = conv(shortcut, channels * 4, kernel=1, stride=1, 
 									use_bias=use_bias, scope='conv_init')
 
-        x = batch_norm(x, is_training, scope='batch_norm_1x1_back')
+        x = batch_norm(x, is_training, channels, scope='batch_4')
         x = relu(x)
         x = conv(x, channels*4, kernel=1, stride=1, use_bias=use_bias, 
 						scope='conv_1x1_back')
@@ -102,20 +105,31 @@ def bottle_resblock(x_init, channels, is_training=True, use_bias=True,
 def conv(x, channels, kernel=4, stride=2, padding='SAME', use_bias=True, scope='conv_0'):
     with tf.variable_scope(scope):
         x = tf.layers.conv2d(inputs=x, filters=channels,
-                             kernel_size=kernel, kernel_initializer=weight_init,
-                             kernel_regularizer=weight_regularizer,
-                             strides=stride, use_bias=use_bias, padding=padding)
-			return x
+                             kernel_size=kernel, kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                             strides=stride, use_bias=use_bias, padding="SAME")
+	return x
 
 def fully_conneted(x, units, use_bias=True, scope='fully_0'):
     with tf.variable_scope(scope):
         x = tf.layers.flatten(x)
         x = tf.layers.dense(x, units=units, kernel_initializer=weight_init, 
-									kernel_regularizer=weight_regularizer, use_bias=use_bias)
+									kernel_regularizer=tf.contrib.layers.xavier_initializer(), use_bias=use_bias)
         return x
 
-def batch_norm(x, is_training=True, scope='batch_norm'):
-    return tf_contrib.layers.batch_norm(x,
-                                        decay=0.9, epsilon=1e-05,
-                                        center=True, scale=True, updates_collections=None,
-                                        is_training=is_training, scope=scope)
+
+def batch_norm(input_layer, is_training, dimension, scope = 'batch_norm'):
+    '''
+    Helper function to do batch normalziation
+    :param input_layer: 4D tensor
+    :param dimension: input_layer.get_shape().as_list()[-1]. The depth of the 4D tensor
+    :return: the 4D tensor after being normalized
+    '''
+    with tf.variable_scope(scope):
+    	mean, variance = tf.nn.moments(input_layer, axes=[0, 1, 2])
+    	beta = tf.get_variable('beta', dimension, tf.float32,
+                               initializer=tf.constant_initializer(0.0, tf.float32))
+    	gamma = tf.get_variable('gamma', dimension, tf.float32,
+                                initializer=tf.constant_initializer(1.0, tf.float32))
+    	bn_layer = tf.nn.batch_normalization(input_layer, mean, variance, beta, gamma, 0.001)
+
+    	return bn_layer
